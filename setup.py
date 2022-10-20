f6e8e27f3b769ba38475c9d3f22ed56d834cc83e
Charles Henden <charles@henden.com.au>
2022-10-20 08:37:40 -0700

    Add packaging file

--- setup.py
+++ setup.py
@@ -7,11 +7,10 @@ setup(
     author='Charles Henden',
     author_email='charles@henden.com.au',
     packages=['gamejam'],
-    scripts=['bin/script1','bin/script2'],
     url='http://pypi.python.org/pypi/gamejam/',
     license='LICENSE.txt',
     description='An OpenGL game jamming framework',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
     install_requires=[
         "freetype_py",
         "glfw",
